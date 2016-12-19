7f591345fa024401c12fb71708536f72906f3341
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-12-18 16:06:33 -0800

    Display new horse form on homepage above button

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-
+  bindEventHandlers();
 });
+
+var bindEventHandlers = function() {
+  $('#add-horse').on('submit', function(event) {
+    event.preventDefault();
+    $('#new-horse-form').removeClass('hidden');
+    $(event.target).addClass('hidden');
+  });
+}
